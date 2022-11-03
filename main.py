dcb100cf71f125d9a7f5c9257af3457e6c476eef
Gyuwon Choi <kafoot@naver.com>
2022-11-03 13:05:14 +0900

    Update tensorboard, model saving

--- main.py
+++ main.py
@@ -23,10 +23,6 @@ def meanSubtract(dataset):
     stdR = np.mean([s[0] for s in std])
     stdG = np.mean([s[1] for s in std])
     stdB = np.mean([s[2] for s in std])
-    
-    print("=========mean & std=========")
-    print(meanR, meanG, meanB)
-    print(stdR, stdG, stdB)
 
     return meanR, meanG, meanB, stdR, stdG, stdB
 
@@ -60,8 +56,8 @@ def get_data(mode):
                                             download=True,
                                             transform=transform)     
         dataloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                                shuffle=False)    
-    
+                                                shuffle=False) 
+
     classes = ('plane', 'car', 'bird', 'cat',
             'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
     
@@ -70,12 +66,16 @@ def get_data(mode):
     
 def train(trainloader, classes):
     
+    # tensorboard 
+    writer = SummaryWriter('./logs/train')
+    
     model = ResNet(arg.layer).to(device)
     summary(model, (3, 32, 32))
     print(summary)
     
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.1, weight_decay= 0.0001, momentum=0.9)
+    optimizer = optim.SGD(model.parameters(), lr= arg.lr , weight_decay= 0.0001, momentum=0.9)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=50)    # lr scheduler : adjust param
 
     epoch_num = arg.epoch
     mini_batch = int(50000 / arg.batch_size)
@@ -94,39 +94,70 @@ def train(trainloader, classes):
             output = model(inputs)
             loss = criterion(output, labels)
             loss.backward()
+            
             optimizer.step()
-
+            
             running_loss += loss.item()
 
             if i % (mini_batch + 1) == mini_batch:    # print every 256 mini-batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / (mini_batch + 1):.3f}')
-                writer.add_scalar("Loss/train", loss, epoch)
+                print(f'[{epoch + 1}, {i + 1:5d}], loss: {running_loss / (mini_batch + 1):.3f}, lr: {optimizer.param_groups[0]["lr"]:.6f}')
+                writer.add_scalar("Loss/train", running_loss / (mini_batch + 1), epoch + 1)
                 running_loss = 0.0
-    
+                
+        scheduler.step(running_loss) # put validation loss
+        
     print('Finished Training')
     
     # model save 
-    model_name_template = './model/'.join(['{}_batch_size', '{}_layer', '{}_epoch', '{}_dataset'])
-    model_name = model_name_template.format(arg.batch_size, arg.layer, arg.epoch, arg.dataset)
-    PATH = model_name.join('.pth')
+    # model_name_template = ''.join(['{}_batch_size', '{}_layer', '{}_epoch', '{}_dataset'])
+    # model_name = model_name_template.format(arg.batch_size, arg.layer, arg.epoch, arg.dataset)
+    PATH = './model/cifar10.pth'
     torch.save(model.state_dict(), PATH)
     print('Finished model saving')
     
-    # tensorboard save
-    PATH = './saved_model/cifar10_resnet.pth'
-
+    writer.close()
     
 def test(testloader, classes):
     print("Start the test!")
     
+    # tensorboard 
+    # writer = SummaryWriter('./logs/test')
+    
+    # model declariation
+    test_model = ResNet(arg.layer).to(device)
+    
+    # should delete the path 
+    # model_name_template = './model/'.join(['{}_batch_size', '{}_layer', '{}_epoch', '{}_dataset'])
+    # model_name = model_name_template.format(arg.batch_size, arg.layer, arg.epoch, arg.dataset)
+    PATH = './model/cifar10.pth'
+    test_model.load_state_dict(torch.load(PATH))
+    
+    # test 
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():
+        for data in testloader:
+            
+            # error : declare CUDA
+            images, labels = data[0].to(device), data[1].to(device)
+            
+            outputs= test_model(images)
+            _, predicted = torch.max(outputs.data, 1) # top-1 : check paper
+            total+= labels.size(0) 
+            correct += (predicted == labels).sum().item()
+            
+    print(f'Accuracy of the network : {100 * correct // total} %')
+    print(f'Error of the network : {100 - (100 * correct // total)} %')
+    
+    # writer.close()
 
 
 def run(): 
     # parse the arguments
-    global arg, device, model, writer
+    global arg, device, model
     
     arg = parser.parse_args()
-    writer = SummaryWriter('./logs')
     
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(device)
@@ -139,11 +170,20 @@ def run():
     test(test_load, classes)
 
 
+# 45k/5k train/val split
+
 # tensorboardx : accuracy, error, loss 
-# test 
-# save model 
-# error % 
+# save model proper
+# print training  accuracy, loss, lr 
+
+# data augmentation : padding, and crop
+
+# percent .xx format 
+
 # add learning scheduler 
+# paper spec 
+
+# validation set??
 
 def main():
     run()
