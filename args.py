e8f650742b9b1f6f5767df520241fb1d0990232a
Gyuwon Choi <kafoot@naver.com>
2022-11-03 13:04:29 +0900

    Add learning rate

--- args.py
+++ args.py
@@ -4,6 +4,6 @@ parser = argparse.ArgumentParser(
         description="Pytorch Implementation for ResNet")
 parser.add_argument('--batch_size', type=int, default=196)
 parser.add_argument('--layer', type=int, default=20)
-parser.add_argument('--epoch', type=int, default=600000)
+parser.add_argument('--epoch', type=int, default=64000)
 parser.add_argument('--dataset', type=str, default ='cifar10')
-
+parser.add_argument('--lr', type=float, default = 0.1)
