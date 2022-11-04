5b822b2a76e06f056945e29319bae1ae59749ec2
Gyuwon Choi <kafoot@naver.com>
2022-11-04 19:48:28 +0900

    Delete batch size and add mini-batch

--- args.py
+++ args.py
@@ -2,9 +2,10 @@ import argparse
 
 parser = argparse.ArgumentParser(
         description="Pytorch Implementation for ResNet")
-parser.add_argument('--batch_size', type=int, default=256)
+# parser.add_argument('--batch_size', type=int, default=256)      # revise 
+parser.add_argument('--mini_batch', type=int, default=128)
 parser.add_argument('--layer', type=int, default=20)
-parser.add_argument('--epoch', type=int, default=64000)
+parser.add_argument('--epoch', type=int, default=500)
 parser.add_argument('--dataset', type=str, default ='cifar10')
 parser.add_argument('--lr', type=float, default = 0.1)
 parser.add_argument('--mode', type=str, default='resume')
