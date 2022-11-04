6c07e41c4ac941db432c68531d47a2d4fef4524e
Gyuwon Choi <kafoot@naver.com>
2022-11-04 14:27:05 +0900

    Add model saving arguments

--- args.py
+++ args.py
@@ -2,8 +2,10 @@ import argparse
 
 parser = argparse.ArgumentParser(
         description="Pytorch Implementation for ResNet")
-parser.add_argument('--batch_size', type=int, default=196)
+parser.add_argument('--batch_size', type=int, default=256)
 parser.add_argument('--layer', type=int, default=20)
 parser.add_argument('--epoch', type=int, default=64000)
 parser.add_argument('--dataset', type=str, default ='cifar10')
 parser.add_argument('--lr', type=float, default = 0.1)
+parser.add_argument('--mode', type=str, default='resume')
+parser.add_argument('--id', type=int, default=0)
