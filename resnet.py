79bfea73d7cf96bee8b206e4d98223f4cb0aa286
Gyuwon Choi <kafoot@naver.com>
2022-11-08 02:20:24 +0900

    Redesingned with nn.ModuleList

--- resnet.py
+++ resnet.py
@@ -9,104 +9,74 @@ class ResNet(nn.Module):
         self.layers = layers - 2
         self.blocks = int(self.layers / 3)
         self.n = int(self.blocks / 2)
-            
-        # === optimize here === #
-        # input channel, output channel, kernel, stride, padding 
-        self.conv1   = nn.Conv2d(3, 16, 3, 1, 1)
-        self.conv2_1 = nn.Conv2d(16, 16, 3, 1, 1)
-        # self.conv2_2 = nn.Conv2d(16, 32, 3, 1, 1)
-        self.conv2_2 = nn.Conv2d(16, 32, 3, 2, 1)                   # subsampling
-        
-        self.conv3_1 = nn.Conv2d(32, 32, 3, 1, 1)
-        # self.conv3_2 = nn.Conv2d(32, 64, 3, 1, 1)
-        self.conv3_2 = nn.Conv2d(32, 64, 3, 2, 1)                   # subsampling
-        
-        self.conv4_1 = nn.Conv2d(64, 64, 3, 1, 1)
-        
-        self.relu = nn.ReLU()
-        
-        self.bn2 = nn.BatchNorm2d(16)
-        self.bn3 = nn.BatchNorm2d(32)
-        self.bn4 = nn.BatchNorm2d(64)
-        
-        self.short2 = nn.Conv2d(16, 32, 1, 2)
-        self.short3 = nn.Conv2d(32, 64, 1, 2)
-        
-        self.maxpool=nn.MaxPool2d(3, 2) 
+
+        self.conv1  = nn.Conv2d(3, 16, 3, 1, 1)
+        self.relu   = nn.ReLU()
+        self.bn2    = nn.BatchNorm2d(16)
         self.avgpool= nn.AvgPool2d(8, 1)
-        self.fc = nn.Linear(64, 10)            # CIFAR-10
- 
+        self.fc = nn.Linear(64, 10)            
+        
+        # ========================================================= #
+        self.conv_ = [nn.Conv2d(16, 16, 3, 1, 1),
+                     nn.Conv2d(32, 32, 3, 1, 1),
+                     nn.Conv2d(64, 64, 3, 1, 1)]
+
+        self.bn_ = [nn.BatchNorm2d(16),
+                   nn.BatchNorm2d(32),
+                   nn.BatchNorm2d(64)]
+        
+        self.sub_sample_ = [
+                           nn.Conv2d(16, 16, 3, 1, 1),
+                           nn.Conv2d(16, 32, 3, 2, 1),
+                           nn.Conv2d(32, 64, 3, 2, 1)]
+        
+        self.short_ = [nn.Conv2d(16, 16, 1, 1),
+                      nn.Conv2d(16, 32, 1, 2),
+                      nn.Conv2d(32, 64, 1, 2)
+                     ]
+                
+        self.conv =  nn.ModuleList(self.conv_)
+        self.bn =  nn.ModuleList(self.bn_)
+        self.sub_sample=  nn.ModuleList(self.sub_sample_)
+        self.short =  nn.ModuleList(self.short_)
 
     def forward(self, x): # miss the x
         
         # conv1 
         x= self.conv1(x)
-        x= self.bn2(x)                          # batch norm before activation 
+        x= self.bn2(x)                          
         
         # conv2~4 block
         for block in range(3):     
-            if block == 0:                      # block 0
-                f = self.conv2_1
-                f2 = self.conv2_2
-                bn = self.bn2                   # 16
-                bn2 = self.bn3                  # 32
-                short = self.short2
-           
-            elif(block == 1):                   # block 1
-                f = self.conv3_1
-                f2 = self.conv3_2
-                bn = self.bn3                   # 32
-                bn2 = self.bn4                  # 64
-                short = self.short3
-            
-            else:                               # block 2
-                f = self.conv4_1
-                bn = self.bn4
-             
+
             # layers 
-            for i in range(self.n):
+            for i in range(self.n):             # reiteration of pair 
                 shortcut = x
-                if(i != (self.n -1)):           # except last pair
-                    for j in range(2):
-                        x = f(x)
-                        
-                        if j==1:                # second layer of a pair 
-                            x = bn(x)            
-                            x = shortcut + x
-                            
-                        x = self.relu(bn(x))
-               
-                else:                               # last pair of block 
-                    # shortcut = x
+            
+                if(i == 0):                             # 1st pair 
+                    x = self.sub_sample[block](x)       # 1st layer of pair 
+                    x = self.relu(self.bn[block](x))
                     
-                    x = f(x)
-                    x = self.relu(bn(x))            # first layer of pair 
+                    x = self.conv[block](x)             # 2nd layer of pair
+                    x = self.bn[block](x)
                     
-                    # if(block!=2):                         
-                    #     x = bn2(f2(x))              # subsampling 
- 
-                    #     shortcut = short(shortcut)  # identity mapping             
-                    #     x = self.relu(shortcut + x)
-                        
-                        
-                    if(block!=2): 
-                    #   x = f(x)
-                    #   x = self.relu(bn(x))        # first layer of pair 
+                    shortcut = self.short[block](shortcut)
+                    x = self.relu(self.bn[block](shortcut + x))
+                
+                else:
+                    x = self.conv[block](x)            # 1st layer of pair 
+                    x = self.relu(self.bn[block](x))
                     
-                      x = bn2(f2(x))              # subsampling 
-                      shortcut = short(shortcut)  # identity mapping             
-                      x = self.relu(shortcut + x)
+                    x = self.conv[block](x)            # 2nd layer of pair
+                    x = self.bn[block](x)
                     
-                    else:                         # last layer of block 2 
-                      x = bn(f(x))
-                      x = shortcut + x
-                      x = self.relu(x)
-        
+                    x = self.relu(self.bn[block](shortcut + x))
+                     
         # conv5 block 
         x = self.avgpool(x)
 
         # fc layer 
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = torch.flatten(x, 1) 
         x = self.fc(x)
         
         return x
